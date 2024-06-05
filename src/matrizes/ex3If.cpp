23adab6ba24d91c4dc01ea3f7b7da90357a9169a
Maria Eduarda <164123811+marias1lva@users.noreply.github.com>
2024-06-05 15:26:47 -0300

    multidimensional arrays exercises

---
+++ src/matrizes/ex3If.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int matriz[4][4], i, j, somatorio = 0; 
+    cout << "Digite 16 valores para preencherem a matriz 4x4: " << endl;
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            cin >> matriz[i][j];
+            if (i != j && i + j != 3){
+                somatorio = somatorio + matriz[i][j];
+            }
+        }
+    }
+    cout << endl << "Matriz: " << endl;
+    for(i = 0; i < 4; i++){
+        for (j = 0; j < 4; j++){
+            cout << matriz[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    cout << "O somario dos valores que NAO estao nas diagonais principal e secundaria eh: " << somatorio << endl;
+    return 0;
+}
+/* EXPLICACAO DO USO DO IF:
+    Diagonal Principal: Na diagonal principal de uma matriz, os índices das linhas e colunas são iguais, ou seja, i == j. Por exemplo, em uma matriz 4x4, os elementos da diagonal principal são:
+    matriz[0][0]
+    matriz[1][1]
+    matriz[2][2]
+    matriz[3][3]
+    
+    Diagonal Secundária: Na diagonal secundária de uma matriz 4x4, a soma dos índices das linhas e colunas é igual ao tamanho da matriz menos 1, ou seja, i + j == 3. Por exemplo, os elementos da diagonal secundária são:
+    matriz[0][3]
+    matriz[1][2]
+    matriz[2][1]
+    matriz[3][0]
+
+    i != j: Este trecho verifica se o elemento não está na diagonal principal.
+*/
